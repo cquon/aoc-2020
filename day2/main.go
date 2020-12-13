884d923222b3b6ac2e0dcb070a9ac79c226da5bf
Corey Quon <corey.quon@gmail.com>
2020-12-12 22:05:04 -0800

    AOC 2020 Day 2 part 2

--- day2/main.go
+++ day2/main.go
@@ -29,6 +29,20 @@ Each line gives the password policy and then the password. The password policy i
 In the above example, 2 passwords are valid. The middle password, cdefg, is not; it contains no instances of b, but needs at least 1. The first and third passwords are valid: they contain one a or nine c, both within the limits of their respective policies.
 
 How many passwords are valid according to their policies?
+
+--- Part Two ---
+While it appears you validated the passwords correctly, they don't seem to be what the Official Toboggan Corporate Authentication System is expecting.
+
+The shopkeeper suddenly realizes that he just accidentally explained the password policy rules from his old job at the sled rental place down the street! The Official Toboggan Corporate Policy actually works a little differently.
+
+Each policy actually describes two positions in the password, where 1 means the first character, 2 means the second character, and so on. (Be careful; Toboggan Corporate Policies have no concept of "index zero"!) Exactly one of these positions must contain the given letter. Other occurrences of the letter are irrelevant for the purposes of policy enforcement.
+
+Given the same example list from above:
+
+1-3 a: abcde is valid: position 1 contains a and position 3 does not.
+1-3 b: cdefg is invalid: neither position 1 nor position 3 contains b.
+2-9 c: ccccccccc is invalid: both position 2 and position 9 contain c.
+How many passwords are valid according to the new interpretation of the policies?
 */
 
 type passwordPolicy struct {
@@ -50,7 +64,19 @@ func (p *passwordPolicy) isValidPassword(password string) bool {
 	return false
 }
 
-func main() {
+type passwordPolicy2 struct {
+	char        byte
+	firstIndex  int
+	secondIndex int
+}
+
+func (p *passwordPolicy2) isValidPassword2(password string) bool {
+	firstIndexMatch := password[p.firstIndex-1] == p.char
+	secondIndexMatch := password[p.secondIndex-1] == p.char
+	return (firstIndexMatch || secondIndexMatch) && !(firstIndexMatch && secondIndexMatch)
+}
+
+func part1() {
 	validPasswords := 0
 	file, err := os.Open("input.txt")
 	if err != nil {
@@ -98,4 +124,59 @@ func main() {
 		}
 	}
 	fmt.Println(validPasswords)
+}
+
+func part2() {
+	validPasswords := 0
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		inputLine := scanner.Text()
+		inputParts := strings.Split(inputLine, ":")
+		if len(inputParts) != 2 {
+			panic("Input malformed")
+		}
+
+		policySection := inputParts[0]
+		policyParts := strings.Split(policySection, " ")
+		if len(inputParts) != 2 {
+			panic("Input malformed")
+		}
+		policyBounds := strings.Split(policyParts[0], "-")
+		if len(inputParts) != 2 {
+			panic("Input malformed")
+		}
+		firstIndex, err := strconv.Atoi(policyBounds[0])
+		if err != nil {
+			panic("Input malformed")
+		}
+		secondIndex, err := strconv.Atoi(policyBounds[1])
+		if err != nil {
+			panic("Input malformed")
+		}
+
+		policy := &passwordPolicy2{
+			char:        policyParts[1][0],
+			firstIndex:  firstIndex,
+			secondIndex: secondIndex,
+		}
+
+		// password after the ":" has a space before it starts
+		password := inputParts[1][1:]
+
+		if policy.isValidPassword2(password) {
+			validPasswords++
+		}
+	}
+	fmt.Println(validPasswords)
+}
+
+func main() {
+	part1()
+	part2()
 }
